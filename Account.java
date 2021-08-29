4432d1a72655b390811eab20785cd7ccc991051d
Nivya Changela <63911199+Nivya-Changela@users.noreply.github.com>
2021-08-29 19:46:50 +0530

    Update Account.java

--- Account.java
+++ Account.java
@@ -4,7 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 public class Account {
-	static String name = "Nivya";
+	static String name;
 	double amountBalance;
 	double withdrawAmount;
 	double depositAmount;
